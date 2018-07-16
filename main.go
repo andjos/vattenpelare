cb8261ffa34925d048a81781d1b0555e89f15681
Anders <anders.josefsson@stena.com>
2018-07-16 18:07:58 +0200

    code clean up

--- main.go
+++ main.go
@@ -12,9 +12,10 @@ func main() {
 		fmt.Println("Ange vattenpelare i meter samt brunnens diameter.")
 		os.Exit(1)
 	}
-	vp, _ := strconv.ParseFloat(os.Args[1], 64) // Vattebpelare
+	pi := float64(3.14159265359)
+	vp, _ := strconv.ParseFloat(os.Args[1], 64) // Vattenpelare
 	bd, _ := strconv.ParseFloat(os.Args[2], 64) // Brunnens diameter
 	br := bd / 2                                // Brunnens radie
-	v := vp * float64(3.14) * br * br * 1000
+	v := vp * pi * br * br * 1000
 	fmt.Println("Vattenmängd i brunnen:", int(v), "Liter.")
 }
