68af8df08ba5f5931b2bca6fb7064ea403a48189
Anders <anders.josefsson@stena.com>
2018-07-16 18:03:00 +0200

    Vi använder nu meter ist. för cm.

--- main.go
+++ main.go
@@ -9,12 +9,12 @@ import (
 func main() {
 
 	if len(os.Args) < 3 {
-		fmt.Println("Ange vattenpelare i cm samt brunnens diameter.")
+		fmt.Println("Ange vattenpelare i meter samt brunnens diameter.")
 		os.Exit(1)
 	}
 	vp, _ := strconv.ParseFloat(os.Args[1], 64) // Vattebpelare
 	bd, _ := strconv.ParseFloat(os.Args[2], 64) // Brunnens diameter
-	br := bd / 100 / 2                          // Brunnens radie
-	v := (vp / 100) * float64(3.14) * br * br * 1000
-	fmt.Println("Vattenmängd i brunnen:", int(v), "Liter")
+	br := bd / 2                                // Brunnens radie
+	v := vp * float64(3.14) * br * br * 1000
+	fmt.Println("Vattenmängd i brunnen:", int(v), "Liter.")
 }
